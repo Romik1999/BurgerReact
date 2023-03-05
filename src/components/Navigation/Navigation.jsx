5db615550ea0b162e67278c78fa2c3ac0157afca
Romik1999 <usankin.roma@gmail.com>
2023-03-06 00:57:36 +0400

    get categories, products from API

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
 import style from "./Navigation.module.css"
 import classNames from "classnames";
 import {Container} from "../Container/Container";
 import {useDispatch, useSelector} from "react-redux";
-import {changeCategory} from "../../store/category/categorySlice.js";
+import {categoryRequestAsync, changeCategory} from "../../store/category/categorySlice.js";
+import {API_URI, POSTFIX} from "../../const.js";
 
 export const Navigation = ({className}) => {
     const {category, activeCategory} = useSelector((state) => state.category);
     const dispatch = useDispatch();
 
+    useEffect(() => {
+        dispatch(categoryRequestAsync('max'));
+    }, [])
 
     return (
         <nav className={style.navigation}>
@@ -16,12 +20,12 @@ export const Navigation = ({className}) => {
                 <ul className={style.list}>
                     {
                         category.map((item, i) => (
-                            <li className={style.item} key={i}>
+                            <li className={style.item} key={item.title}>
                                 <button className={classNames(
                                     style.button,
                                     activeCategory === i ? style.button_active : ''
                                 )}
-                                        style={{backgroundImage: `url(${item.image})`}}
+                                        style={{backgroundImage: `url(${API_URI}/${item.image})`}}
                                         onClick={() => {
                                             dispatch(changeCategory({indexCategory: i}))
                                         }}
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import categoryReducer from "./category/categorySlice.js";
+import productReducer from "./product/productSlice.js";
 
 
 export const store = configureStore({
     reducer: {
-        category: categoryReducer
+        category: categoryReducer,
+        product: productReducer,
     }
 });
